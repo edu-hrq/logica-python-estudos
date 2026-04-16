edbe15aef8dc61af60a93e7b39f7f191a703867d
edu-hrq <eduardohenriquedasilva1096@gmail.com>
2026-04-16 18:24:45 -0300

    Feat: Separação adequada dos projetos

---
+++ projetos/calculadora/01_README.md
@@ -0,0 +1,44 @@
+# CALCULADORA SIMPLES EM PYTHON
+
+## Descrição:
+
+Esse projeto é uma calculadora simples desenvolvida na linguagem Python com o foco de treinar lógica de programação e estruturas de controle.
+O programa permite o user realizar operações matemáticas básicas diretamente pelo terminal, com um menu interativo, histórico das operações matemáticas realizadas e validações de entrada.
+
+## Funcionalidades:
+
+- Adição
+- Subtração
+- Multiplicação
+- Divisão (com tratamento para divisão por zero)
+- Potenciação
+- Visualização de histórico de cálculos feitos
+- Limpeza do histórico
+- Menu interativo
+
+## Como usar:
+
+1. Execute o arquivo 'calculadora-simples.py'
+2. Escolha a operação desejada
+3. Insira os números solicitados
+4. Veja o resultado
+5. Repita
+
+## Tecnologias usadas:
+
+- Python 3
+
+## Aprendizados:
+
+- Estruturas condicionais (if, elif e else);
+- Laços de repetição (while);
+- Entrada e saída de dados;
+- Validação de dados;
+- Organização e fluxo de programa;
+- Estrutura de dados - Listas e Dicionários.
+
+## Pontos a melhorar futuramente:
+
+- Futura interface gráfica
+- Adicionar data/hora
+- Salvamento em arquivo .txt
---
+++ projetos/calculadora/main.py
@@ -0,0 +1,76 @@
+# CALCULADORA SIMPLES - Objetivo com o código: Receber dois números, realizar uma operação e mostrar o resultado.
+def adicao(a, b): # função de adição
+        return a + b
+def subtracao(a, b): # função de subtração
+        return a - b
+def multiplicacao(a, b): # função de multiplicação
+        return a * b
+def divisao(a, b): # função de divisão (que valida se o divisor é 0 ou não)
+        if b == 0:
+            return "Erro: Divisão por 0 não é permitida."
+        else:
+            return a / b
+def potenciacao(a, b): # função de potenciação
+        return a ** b
+
+historico = []
+
+operacoes = { # dicionario das operacoes que serão feitas na calculadora
+        1: adicao,
+        2: subtracao,
+        3: multiplicacao,
+        4: divisao,
+        5: potenciacao,
+    }
+
+simbolos = {
+      1: '+',
+      2: '-',
+      3: '*',
+      4: '/',
+      5: '**',  
+    }
+
+while True:
+    print('\n======== CALCULADORA ========\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Potenciação\n6 - Ver histórico\n7 - Limpar histórico\n0 - Sair\n=============================')
+    try:
+        operacao = int(input('Escolha uma opção: '))
+    except ValueError:
+        print('Digite apenas números!')
+        continue
+
+    if operacao == 0: # opção p/ sair da calculadora
+        print('Encerrando calculadora...')
+        break
+    
+    if operacao == 6: # ver histórico
+          print('HISTÓRICO:')
+          if not historico:
+                print('Nenhuma operação realizada.')
+          else:
+                for item in historico:
+                      print(item)
+          continue
+
+    if operacao == 7: # limpar o histórico
+          historico.clear()
+          print('Histórico apagado!')
+          continue
+    
+    if operacao not in operacoes:
+         print('Operação inválida! Tente novamente: ')
+         continue
+    
+    try: # emtrada de dados
+        numero = float(input('Digite o primeiro número desejado: '))
+        numero2 = float(input('Digite o segundo número desejado: '))
+    except ValueError:
+          print('Digite valores numéricos!')
+          continue
+    
+    resultado = operacoes[operacao](numero, numero2) # processamento dos dados
+
+    simbolo = simbolos[operacao] # guarda a operação selecionada
+    historico.append(f'{numero} {simbolo} {numero2} = {resultado}') # adiciona cálculo feito ao histórico
+    
+    print(f'\nResultado da operação desejada: {resultado}\n') # imprime o resultado dos dados
